aacbf5c39217313ff3f167cd1ceb716c88c3c043
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-06-11 00:12:53 +0300

    Добавляет структуру компонентов

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Component } from 'react';
+
+import { Section } from './components/Section';
+import { FeedbackOptions } from './components/FeedbackOptions';
+
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Section title="Please leave feedback">
+          <FeedbackOptions></FeedbackOptions>
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export class FeedbackOptions extends Component {
+  render() {
+    return <div>FeedbackOptions</div>;
+  }
+}
